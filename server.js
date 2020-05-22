e1da8c6a39b26abc49f56cbe4eb37fd6ea14cf61
dev-bondoor <yocheved@food2you.com>
2020-05-22 13:01:51 +0300

    comment

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/contacts'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/contacts/index.html'));
+// res.sendFile(path.join(__dirname+'/dist/contacts/index.html'));
+// });
+
+app.get('*', (req, res) => {
+  res.sendFile(`./contacts/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
-console.log('Node-express server listening!' , __dirname);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
