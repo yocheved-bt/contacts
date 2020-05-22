e593f00745aa63c6dad457da5973c0e61c82bcec
dev-bondoor <yocheved@food2you.com>
2020-05-22 13:32:50 +0300

    update cli version

--- server.js
+++ server.js
@@ -7,14 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/contacts'));
 
-// app.get('/*', function(req,res) {
+ app.get('/*', function(req,res) {
 
-// res.sendFile(path.join(__dirname+'/dist/contacts/index.html'));
-// });
-
-app.get('*', (req, res) => {
-  res.sendFile(`./contacts/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-});
-
+ res.sendFile(path.join(__dirname+'/dist/contacts/index.html'));
+ });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
